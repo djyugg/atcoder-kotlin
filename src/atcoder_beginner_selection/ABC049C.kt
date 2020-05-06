7d9db08f3bba61fce63ef68bdf6012671cc62276
djyugg <9217500+djyugg@users.noreply.github.com>
2020-05-06 18:14:36 +0900

    AtCoder Beginners Selection ABC049C

--- src/atcoder_beginner_selection/ABC049C.kt
+++ src/atcoder_beginner_selection/ABC049C.kt
@@ -1,32 +1,15 @@
 fun main(args: Array<String>) {
     var S = readLine()!!
 
-    val dream = "dream"
-    val dreamer = "dreamer"
-    val erase = "erase"
-    val eraser = "eraser"
-
+    val texts = arrayOf("dreamer", "eraser", "dream", "erase")
     while (true) {
-        if (S == "") {
+        if(S == "") {
             println("YES")
             return
         }
 
-        S = if (S.startsWith(dream)) {
-            if (S.startsWith(dreamer) && !S.substring(dream.length).startsWith(erase)) {
-                S.substring(dreamer.length)
-            } else {
-                S.substring(dream.length)
-            }
-        } else if (S.startsWith(erase)) {
-            if (S.startsWith(eraser) && !S.substring(erase.length).startsWith(dream)) {
-                S.substring(eraser.length)
-            } else {
-                S.substring(erase.length)
-            }
-        } else {
-            break
-        }
+        val matchText = texts.find { S.endsWith(it) } ?: break
+        S = S.substring(0 , S.length - matchText.length)
     }
 
     println("NO")
